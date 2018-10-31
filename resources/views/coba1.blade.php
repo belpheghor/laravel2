a983f236aa1b6b89f92185d9cded8f37158a3b8b
ggantara <g.gantara@mail.ugm.ac.id>
2018-10-31 13:25:17 +0700

    blade templating

---
+++ resources/views/coba1.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+@section('content-header')
+<h1>
+	Coba
+	<small>control panel</small>
+</h1>
+<ol class="breadcrumb">
+	<li><a href="/"><i class="fa fa-dashboard"></i> HOME</a></li>
+	<li class="active">Coba 1</li>
+</ol>
+@endsection
+@section('content')
+Halo ini coba 1
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('pesan', function () {
+    return view('pesan');
+});
+
+Route::post('pesan/kirim', function () {
+    return view('pesan_kirim');
+});
+
+Route::get('template', function () {
+    return view('layouts.master');
+});
+Route::get('coba1', function () {
+    return view('coba1');
+});
+Route::get('coba2', function () {
+    return view('coba2');
+});
